c423dca2428853f907d272b83fbac82e5e96035d
Esteban Alcudia <alcudia.erickesteban@gmail.com>
2025-11-23 20:48:35 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyectoparcial3EA',
+  webDir: 'www'
+};
+
+export default config;
